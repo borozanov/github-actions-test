b81b4dc032c22ad13ab575052b6eeb5d06940e8a
borozanov <borozanov@gmail.com>
2023-08-10 10:30:24 +0200

    Create sample_code.py

---
+++ sample_code.py
@@ -0,0 +1,9 @@
+import numpy as np
+
+def print_transpose(a):
+    a = np.array(a)
+    print(train_orig[0, 0], '\n', trainX[0, :])
+
+
+if __name__ == '__main__':
+    print_transpose([[1,2,3], [4,5,6]])
